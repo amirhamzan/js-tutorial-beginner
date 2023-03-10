d984dc4e7563d5b80652c7789fc44ffdb23740f4
Amir Hassan <amirhassan.hamzan@gmail.com>
2023-03-11 07:40:33 +0800

    Ways to initialize variables

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ console.log("This is test logging");
 // cannot start with number(eg: 1name)
 // cannot contain space or hyphen(eg: 'first name', 'first-name')
 // are case sensitive, (eg:firstName != FirstName) BEST PRACTICES USE CAMEL CASE
+
+// =====ways to initialize variables=====
+let firstName, lastName;
+let firstAddress = 'House No. 12', secondAddress;
+let firstNumberPhone = '0129090123';
+let secondNumberPhone = '0129090555';
+
+// console.log(firstName, lastName, firstAddress, secondAddress, firstNumberPhone, secondNumberPhone);
