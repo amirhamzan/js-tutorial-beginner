7a986231fb0c337acacf08d26e0c2fca07088e4d
Amir Hassan <amirhassan.hamzan@gmail.com>
2023-03-11 07:29:00 +0800

    Rules declaring js variables

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("This is test logging");
+
+// =====rules of declaring js variables======
+// cannot be reserve keywords(eg: let, if, null)
+// should be meaningful(eg: name, address)
+// cannot start with number(eg: 1name)
+// cannot contain space or hyphen(eg: 'first name', 'first-name')
+// are case sensitive, (eg:firstName != FirstName) BEST PRACTICES USE CAMEL CASE
